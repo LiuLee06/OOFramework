c1499288a4dc4c22449bd876fde90fb4815ca371
LiuLee <15688499668@163.com>
2026-01-27 21:53:27 +0800

    Keep only OOFramework.jar file

--- src/com/oo/framework/API/Controllable.java
+++
@@ -1,9 +0,0 @@
-package com.oo.framework.API;
-
-public interface Controllable {
-    void moveUp();
-    void moveDown();
-    void moveLeft();
-    void moveRight();
-}
-
--- src/com/oo/framework/API/Shape.java
+++
@@ -1,7 +0,0 @@
-package com.oo.framework.API;
-
-import java.awt.*;
-
-public interface Shape {
-    void drawMyself(Graphics g);
-}
